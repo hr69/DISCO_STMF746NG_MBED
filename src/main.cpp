591ea81eeb501985df1db889707c7a04385d0b76
unknown <hr69@hw.ac.uk>
2015-09-23 19:03:45 +0400

    removed unnecessary includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,33 @@
 #include "mbed.h"
+void  MX_GPIO_Init (void);
 
+//Serial pc(USBTX, USBRX); // tx, rx
 DigitalOut myled(LED1);
 DigitalIn myswitch(PI_11);
 int main() {  
-    while(1) {
-    	if(myswitch) myled = 1;
-    	else myled = 0;
-        }
+
+	MX_GPIO_Init ();
+    while(true) {
+    	HAL_GPIO_TogglePin (GPIOI, GPIO_PIN_1);
+    	wait(0.5);
+     }
+
+    return 0;
+}
+
+
+
+void  MX_GPIO_Init (void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO ports Clock Enable */
+  __GPIOI_CLK_ENABLE ();
+
+  /* Configure GPIO pin: PI1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init (GPIOI, & GPIO_InitStruct);
 }
